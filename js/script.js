cf3f2d605883ddef2a0723bdc3941d065c4e8a9c
Itumeleng1307 <tumimbangi@gmail.com>
2025-08-15 17:13:06 +0200

    Added js for Typewriter for recipe-typewriter

---
+++ js/script.js
@@ -0,0 +1,13 @@
+let typewriter = new Typewriter('#recipe-typewriter', {
+  autoStart: true,
+  loop: false,
+  delay: 20,
+});
+
+typewriter
+  .typeString("Where Smoke Meets Flavour. It's more than a meal, it's a vibe!")
+  .pauseFor(500)
+  .typeString("<br>Grilled meats, spicy sides, and good times shared around the fire.")
+  .pauseFor(500)
+  .typeString("<br>Authentic South African flame culture, served hot.")
+  .start();
